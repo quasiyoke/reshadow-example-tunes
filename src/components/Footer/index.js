e2cf390a4bcdd0b3e1c25ef51351722067154748
Petr Ermishkin <quasiyoke@gmail.com>
2019-05-04 19:17:32 +0300

    05-sticky-footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import styled from 'reshadow';
+import styles from './styles.css';
 
-export default () => (
+export default () => styled(styles)(
   <footer>
     ;)
   </footer>
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import styled from 'reshadow';
 import Header from '../Header';
 import Footer from '../Footer';
+import styles from './styles.css';
 
 export default ({
   children,
-}) => (
+}) => styled(styles)(
   <>
     <Header />
-    {children}
+    <content>
+      {children}
+    </content>
     <Footer />
   </>
 );
